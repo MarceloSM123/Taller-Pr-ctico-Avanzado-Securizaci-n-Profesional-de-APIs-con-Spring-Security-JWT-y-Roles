4cee0a01b820ea12a61029396640d2705f4c6e0f
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-06-07 00:55:27 -0500

    Hito 3: 3.3 Configuración Central de Seguridad (SecurityConfig.java)

---
+++ proyectos/src/main/java/com/krakedev/proyectos/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.krakedev.proyectos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration //anotacion que indica configuraciones importantes que deben ser analizadas al iniciar el sistema
+@EnableMethodSecurity //anotacion que activa la seguridad basada en metodos nos permite usar anotaciones SpringSecurity
+public class SecurityConfig {
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		super();
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+
+
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    return http.csrf(csrf -> csrf.disable()) // apagamos la cookies 
+	        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/login", "/auth/registrar").permitAll()
+	        .anyRequest().authenticated())
+	        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
+	}
+}
